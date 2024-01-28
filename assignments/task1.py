494807cefa5f53e9f724cfa39bed54a0219f9a62
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2024-01-28 11:56:05 +0800

    finished task 1

---
+++ assignments/task1.py
@@ -0,0 +1,9 @@
+# Get user input for the length and width of the rectangle
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area of the rectangle
+area = length * width
+
+# Print the calculated area
+print(f"The area of the rectangle is: {area}")
---
+++ main.py
@@ -0,0 +1,11 @@
+# print("hello world")
+# print(177 * 8)
+# print(177 + 8)
+
+# print("hey", 6,7,sep="~",end="end")
+
+# a = 1
+# print(type(a))
+
+list = [1, 2, [-1, -2], ["apple", "banana"]]
+print(list)
