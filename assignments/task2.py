18af186423634ef053a4de637c8edcd3076a5def
ShahrulSaifuddin <shahrul_saifuddin@yahoo.com>
2024-01-28 12:50:02 +0800

    finished task 2

---
+++ assignments/task2.py
@@ -0,0 +1,17 @@
+# Prompt the user to enter a string
+user_input = input("Enter a string: ")
+
+# Print the length of the entered string
+print("Length of the string:", len(user_input))
+
+# Print the string in uppercase
+print("Uppercase:", user_input.upper())
+
+# Print the string in lowercase
+print("Lowercase:", user_input.lower())
+
+# Print the first character of the string
+print("First character:", user_input[0])
+
+# Print the last character of the string
+print("Last character:", user_input[-1])
